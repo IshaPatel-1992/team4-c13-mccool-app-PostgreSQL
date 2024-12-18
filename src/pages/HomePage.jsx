4592d844f1e712e0e28e48e856d83a679697e088
IshaPatel-1992 <patelisha1992@gmail.com>
2024-12-18 12:15:05 -0700

    test git repo is working

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <>
       <Header />
-      { /* <h1>This is our Home Page...</h1>
+      { /* <h1>This is our Home Page... I want to check repo is working</h1>
       <h2>This Home Page will be the first viewing page for this app</h2>
       <h3>This page will contain Quick Search Bar, User can able to search quick tips about companssionate leadership</h3>--> */ }
       <QuickSearchBar />
